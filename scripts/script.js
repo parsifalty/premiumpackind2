46ef7882517037121ec907472ddf2fdb5041be31
Pasha Litvak <parsifalty@gmail.com>
2023-03-23 21:47:30 +0200

    posdjfosifj

--- scripts/script.js
+++ scripts/script.js
@@ -12,23 +12,51 @@ let headerListList = document.querySelector('.header__list-list');
 headerEquipment.addEventListener('click', function(){
 headerEquipmentList.classList.toggle('header__list-list_visibility');
 headerEquipment.classList.toggle('header__list-link_active');
-});
+function equipmentName(){
+      if (headerEquipment.textContent === 'Оборудование ⬆️') { 
+            headerEquipment.textContent = 'Оборудование ⬇️'
+                 }
+                 else {
+                        headerEquipment.textContent = 'Оборудование ⬆️'
+           }
+               }
+             equipmentName();
+  });
 
 headerServices.addEventListener('click', function(){
       headerServicesList.classList.toggle('header__list-list_visibility');
       headerServices.classList.toggle('header__list-link_active');
+      function servicesName(){
+            if (headerServices.textContent === 'Услуги ⬆️') { 
+       headerServices.textContent = 'Услуги ⬇️'
+            }
+            else {
+                   headerServices.textContent = 'Услуги ⬆️'
+      }
+          }
+        servicesName();
 });
 
 headerMaterials.addEventListener('click', function(){ 
       headerMaterialsList.classList.toggle('header__list-list_visibility');
       headerMaterials.classList.toggle('header__list-link_active');
+      function materialsName(){
+            if (headerMaterials.textContent === 'Материалы ⬆️') { 
+       headerMaterials.textContent = 'Материалы ⬇️'
+            }
+            else {
+                   headerMaterials.textContent = 'Материалы ⬆️'
+      }
+          }
+        materialsName();
 });
 
 let headerLanguage = document.querySelector('.header__language');
 let headerLanguageList = document.querySelector('.header__language-list');
 
-headerLanguage.addEventListener('click', function(){ 
+/* headerLanguage.addEventListener('click', function(){ 
       headerLanguageList.classList.toggle('header__list-list_visibility');
+      headerLanguage.classList.toggle('header__list-link_active');
       console.log(headerLanguageList);
-});
+}); */
 
